e6a80c7744f440c0adfcd26397459ad617f90a81
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-02-22 23:25:24 +0530

    Add initial present route with basic textarea component

---
+++ src/routes/present/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import type { PageData } from './$types'
+
+let { data }: { data: PageData } = $props()
+</script>
+
+<section class="m">
+<textarea name="" id=""></textarea>
+</section>
