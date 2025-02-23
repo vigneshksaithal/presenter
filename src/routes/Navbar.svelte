201fa411648af2a6b8c7668ec91863c2ebaeeff2
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-02-23 18:01:51 +0530

    Reorder words in Navbar title

--- src/routes/Navbar.svelte
+++ src/routes/Navbar.svelte
@@ -1,6 +1,6 @@
 <nav class="max-w-xl mx-auto px-4">
     <ul>
-      <li><strong>Presentation + Research + Voice</strong></li>
+      <li><strong>Research + Presentation + Voice</strong></li>
     </ul>
     <!-- <ul>
       <li><a href="/about">About</a></li>
