6eb0991c4e21421bcbe553ab362e5c75835606c1
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-03-08 11:30:30 +0530

    Add PostHog page navigation tracking
    - Implement page navigation event tracking with PostHog
    - Add beforeNavigate and afterNavigate hooks in +layout.svelte
    - Simplify PostHog initialization in +layout.ts
    - Remove onMount and use load function for PostHog setup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 import '../app.css'
 let { children } = $props()
+
+import { browser } from '$app/environment';
+  import { beforeNavigate, afterNavigate } from '$app/navigation';
+  import posthog from 'posthog-js'
+
+  if (browser) {
+    beforeNavigate(() => posthog.capture('$pageleave'));
+    afterNavigate(() => posthog.capture('$pageview'));
+  }
 </script>
 
 {@render children()}
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,16 +1,13 @@
 import posthog from 'posthog-js'
 import { browser } from '$app/environment';
-import { onMount } from 'svelte';
 
-onMount(() => {
+export const load = async () => {
+
   if (browser) {
     posthog.init(
       'phc_ICVutv55XS8VyMlB2c93KDcvpNUaRoTRukSuYTdoGeN',
-      { 
-        api_host: 'https://us.i.posthog.com',
-        person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
-      }
+      { api_host: 'https://us.i.posthog.com' }
     )
   }
   return
-});
+};
